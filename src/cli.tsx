a0ff57a2a11ba0747cb4e8e4d660ef4dfbd793d8
wu-wilson <wilson.cui.wu@gmail.com>
2026-01-14 11:18:34 -0600

    updated repo structure

--- src/cli.tsx
+++ src/cli.tsx
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import {render} from 'ink';
-import App from './app';
+import { render } from "ink";
+import App from "./components/App";
 
 render(<App />);
---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import { Text } from "ink";
+
+const App = () => {
+  return (
+    <Text>
+      Hello <Text color="green">Stranger</Text>
+    </Text>
+  );
+};
+
+export default App;
