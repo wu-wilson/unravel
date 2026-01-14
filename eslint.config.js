f59e248c7c63bb9b4d4d80d47bbf5fdb41f29d70
wu-wilson <wilson.cui.wu@gmail.com>
2026-01-14 09:10:28 -0600

    removed unused packages, set up eslint

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import reactHooks from 'eslint-plugin-react-hooks';
+import react from 'eslint-plugin-react';
+
+export default {
+	ignorePatterns: ['dist'],
+	files: ['**/*.{ts,tsx}'],
+	env: {
+		node: true,
+		es2021: true,
+	},
+	plugins: {
+		react,
+		'react-hooks': reactHooks,
+	},
+	extends: ['plugin:react/recommended'],
+	rules: {
+		...reactHooks.configs.recommended.rules,
+		'react/prop-types': 'off',
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+};
